d7d8bac3ead6fdd70e341d1ec1bae509ba652470
Lucas Feliciano <162584840+Felici4no@users.noreply.github.com>
2024-09-10 10:30:34 -0300

    Projeto funcional

--- src/function.js
+++ src/function.js
@@ -1,3 +1,27 @@
+var nivel = 1;
+var atividade = 1;
+var gab = 0;
+
+//Conquista
+const conquista = [
+    "Medalha de Principiante da Aritmética",
+    "Troféu de Somador Rápido",
+    "Distintivo de Subtração Precisa",
+    "Medalha de Multiplicador Experiente",
+    "Escudo do Divisor Mestre",
+    "Troféu de Cálculo Mental Rápido",
+    "Medalha de Expert em Operações Básicas",
+    "Distintivo de Aritmética Avançada",
+    "Escudo do Prodigio Aritmético",
+    "Troféu de Campeão da Tabuada",
+    "Medalha de Mestre das Quatro Operações",
+    "Distintivo do Desafio de Números",
+    "Medalha de Virtuoso da Multiplicação",
+    "Troféu de Genialidade Aritmética",
+    "Escudo de Herói dos Números Inteiros"
+];
+
+
 function contentsMath() {
     // Armazena um valor no localStorage
     localStorage.setItem('startMath', true);
@@ -14,17 +38,110 @@ function arithmeticMath() {
     window.location.href = "classroom.html";
 }
 
+function start() {
+    // Apresenta Nível e Atividade
+    var nivelHtml = document.getElementById('nNivel');
+    nivelHtml.innerHTML = nivel;
+
+    var atividadeHtml = document.getElementById('nAtividade');
+    atividadeHtml.innerHTML = atividade;
+
+    // Gera números aleatórios
+    min = 0;
+    max = 10 * nivel; // Ajusta de acordo com o nível
+    y = gerarNumeroAleatorio(min, max);
+    x = gerarNumeroAleatorio(min, max);
+    r = y * x;
+
+    // Monta a questão
+    montaQuestao(y, x, r);
+}
+
+function montaQuestao(y, x, r) {
+    // Adiciona a questão no enunciado
+    var questao = document.getElementById('questao');
+    questao.innerHTML = `${y} x ${x} = ?`;
+
+    // Escolhe onde a resposta vai ficar
+    gab = gerarNumeroAleatorio(1, 4);
+
+    // Mostra nas opções
+    var a = document.getElementById('btn_a');
+    var b = document.getElementById('btn_b');
+    var c = document.getElementById('btn_c');
+    var d = document.getElementById('btn_d');
+
+    // Gerar opções
+    if (gab == 1) {
+        a.innerHTML = r;
+        b.innerHTML = r + gerarNumeroAleatorio(5, 8);
+        c.innerHTML = r - gerarNumeroAleatorio(1, 2);
+        d.innerHTML = r + gerarNumeroAleatorio(3, 4);
+    } else if (gab == 2) {
+        a.innerHTML = r - gerarNumeroAleatorio(1, 2);
+        b.innerHTML = r;
+        c.innerHTML = r + gerarNumeroAleatorio(1, 2);
+        d.innerHTML = r + gerarNumeroAleatorio(3, 4);
+    } else if (gab == 3) {
+        a.innerHTML = r + gerarNumeroAleatorio(1, 2);
+        b.innerHTML = r - gerarNumeroAleatorio(1, 2);
+        c.innerHTML = r;
+        d.innerHTML = r + gerarNumeroAleatorio(3, 4);
+    } else if (gab == 4) {
+        a.innerHTML = r - gerarNumeroAleatorio(1, 2);
+        b.innerHTML = r + gerarNumeroAleatorio(5, 8);
+        c.innerHTML = r + gerarNumeroAleatorio(1, 2);
+        d.innerHTML = r;
+    } else {
+        console.log('Erro no gerar questões');
+    }
+}
+
 document.addEventListener("DOMContentLoaded", function() {
-    // Verifica se o item "startMath" existe no localStorage
-    var start = localStorage.getItem('startMath');
+    start();
 });
-
-
-
-
-
 
 function gerarNumeroAleatorio(min, max) {
     // Gera um número aleatório entre min e max (inclusivo)
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function escolha(resposta) {
+    
+    if (resposta == ''){ // 
+        if(atividade <= 0 ){
+            atividade = 0;
+        }else{
+            atividade--;
+        }
+        return start();
+    }
+    else if (resposta == 'a' && gab == 1 || resposta == 'b' && gab == 2 || resposta == 'c' && gab == 3 || resposta == 'd' && gab == 4) {
+        atividade++;
+        resposta ='';
+        
+        if (atividade % 10 === 0) {
+            nivel ++;  // Incrementa o nível
+        }
+        classific(atividade,nivel);
+        return start(); // Carrega a próxima pergunta
+    } else {
+        //alert('Errou! Tente novamente.');
+    }
+}
+
+// Adiciona eventos de clique aos botões
+document.getElementById('btn_a').addEventListener('click', function() { escolha('a'); });
+document.getElementById('btn_b').addEventListener('click', function() { escolha('b'); });
+document.getElementById('btn_c').addEventListener('click', function() { escolha('c'); });
+document.getElementById('btn_d').addEventListener('click', function() { escolha('d'); });
+
+function classific(atividade,nivel){
+    // Pega todos os elementos <strong>
+    var allStrongTags = document.querySelectorAll('p strong');
+
+    // Pega o texto do primeiro strong (primeira <p>)
+    allStrongTags[0].textContent = conquista[nivel] // "Medalha de Principiante"
+
+    allStrongTags[1].textContent = 15 * atividade; // "0"
 }
